7019cddc7e365a9abe61fe7fd3792ddd36d26f07
Joel Hobgood <jhobgoo@gmail.com>
2020-04-05 01:49:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,6 +1,7 @@
 class String
 
   def sentence?
+<<<<<<< HEAD
     self.end_with?(".")
   end
 
@@ -14,6 +15,27 @@ class String
 
   def count_sentences
     self.split(/[.?!]+/).count
+=======
+    self.sentence?
+  end
+
+  def question?
+    self.question?
+  end
+
+  def exclamation?
+    self.exlamation?
+  end
+
+  def count_sentences
+    split_array = []
+    self.collect do |string|
+      self.split(".")
+      self.split("?")
+      self.split("!")
+    end
+    split.array
+>>>>>>> af618ab858d242c63dc55f0fe85581497eecb75e
   end
   
 end
